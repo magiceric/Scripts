a3b4e4556908093ecde1be939c9056e8af68acda
Вячеслав Цыкин <tsykin.vyacheslav@otr.ru>
2015-03-16 00:29:16 +0300

    Reformat code

--- getFirstKinopoiskMovie.py
+++ getFirstKinopoiskMovie.py
@@ -8,22 +8,26 @@ import xml.etree.ElementTree as ET
 
 HOST = 'http://rating.kinopoisk.ru/'
 
+
 def requestURL(url):
     response = urllib.request.urlopen(url).read()
     return response.decode('utf-8')
+
 
 def getRating(string):
     xml = ET.fromstring(string)
     rating = xml.find('kp_rating').text
     return rating
 
+
 def main():
-    for id in range (1, 1000):
+    for id in range(1, 1000):
         url = HOST + repr(id) + ".xml"
         rating = getRating(requestURL(url))
-        if (rating != '0'):
+        if rating != '0':
             print("CONGRATULATION! First KP movie is: " + repr(id) + ", rating = " + rating)
-            print("http://kinopoisk.ru/film/"+repr(id))
+            print("http://kinopoisk.ru/film/" + repr(id))
             break
+
 
 main()
